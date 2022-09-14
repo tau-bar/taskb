0279daccba485a7beb835a9399448d4ae242f13a
tau-bar <taufiq.b.ar@gmail.com>
2022-09-14 22:46:41 +0800

    Test travis

--- url-service/config/db.config.js
+++ url-service/config/db.config.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV !== "production") {
 const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abnpawu.mongodb.net/?retryWrites=true&w=majority`
 const LOCAL_DB_URI = 'mongodb://localhost:27017/urlshortener'
 
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV == 'test') {
     mongoose.connect(LOCAL_DB_URI, {
         useNewUrlParser: true,
