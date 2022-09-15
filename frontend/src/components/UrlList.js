82b8ed2311f096c5f20c8d437cedfbba8d17dea5
tau-bar <taufiq.b.ar@gmail.com>
2022-09-16 03:23:17 +0800

    Complete frontend

--- frontend/src/components/UrlList.js
+++ frontend/src/components/UrlList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -6,22 +6,86 @@ import ListItemText from '@mui/material/ListItemText';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { PRODUCTION_BASE_URL } from '../utils/const';
+import { Box, Button, Modal, TextField, Typography } from '@mui/material';
 
-const UrlList = () => {
+const EditModal = ({ open, handleUpdate, onClose }) => {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [newUrl, setNewUrl] = useState('');
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Update URL:
+        </Typography>
+        <TextField
+                onChange={(e) => setNewUrl(e.target.value)}
+                sx={{ my:2 }}
+                fullWidth
+                id="newUrl"
+                label="New URL"
+                name="newUrl"
+        />
+        <Button
+            fullWidth
+            onClick={() => {
+              handleUpdate(newUrl)
+              onClose()
+            }}
+            variant="contained"
+            sx={{ width: 200, mt: 3, mb: 2 }}
+          >
+            Update
+          </Button>
+      </Box>
+    </Modal>
+  )
+}
+
+const UrlList = ({ list, handleDelete, handleEdit }) => {
+  const [open, setOpen] = useState(false);
+  const [editCode, setEditCode] = useState('');
+  const [editIdx, setEditIdx] = useState(-1);
+
+  const handleClickEdit = (ec, idx) => {
+    setOpen(true);
+    setEditCode(ec);
+    setEditIdx(idx);
+  }
+
   return (
     <List sx={{ border: 1, borderColor: 'primary.main', width: '100%', bgcolor: 'background.paper' }}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
+      <EditModal open={open} handleUpdate={(newUrl) => handleEdit(editCode, editIdx, newUrl)} onClose={() => setOpen(false)}/>
+      {list.map((value, idx) => {
+        const url = `${PRODUCTION_BASE_URL}/api/url/${value.urlCode}`;
+        const labelId = `checkbox-list-label-${value.id}`;
 
         return (
           <ListItem
-            key={value}
+            key={value.id}
             secondaryAction={
               <div>
-              <IconButton edge="end" aria-label="edit">
+              <IconButton edge="end" aria-label="edit" onClick={() => handleClickEdit(value.urlCode, idx)}>
                 <EditIcon />
               </IconButton>
-              <IconButton edge="end" aria-label="delete">
+              <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(value.urlCode, idx)}>
                 <DeleteIcon />
               </IconButton>
               </div>
@@ -30,7 +94,7 @@ const UrlList = () => {
             disablePadding
           >
             <ListItemButton role={undefined} dense>
-              <ListItemText id={labelId} primary={`URL ${value}`} secondary={'actual url'}/>
+              <ListItemText id={labelId} primary={<a href={url}>{url}</a>} secondary={value.longUrl}/>
             </ListItemButton>
           </ListItem>
         );
--- frontend/src/pages/UrlListPage.js
+++ frontend/src/pages/UrlListPage.js
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UrlList from "../components/UrlList";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Box, Container } from "@mui/system";
-import { CssBaseline, Typography } from "@mui/material";
+import { Box, Container, Stack } from "@mui/system";
+import { Alert, CssBaseline, Typography } from "@mui/material";
+import axios from "axios";
+import { PRODUCTION_BASE_URL } from "../utils/const";
 
 const theme = createTheme();
 
-const UrlListPage = () => {
+
+
+
+
+
+
+const UrlListPage = () => {  
+  const [list, setList] = useState([]);
+  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    handleGetList();
+  }, []);
+
+  const handleGetList = () => {
+    setError('');
+    setMessage('');
+    axios.get(`${PRODUCTION_BASE_URL}/api/url/all`)
+    .then(res => {
+      setList(res.data)
+    }).catch((err) => {
+      setError(err);
+    })
+  }
+
+  const handleDelete = (code, idx) => {
+    setError('');
+    setMessage('');
+    axios.delete(`${PRODUCTION_BASE_URL}/api/url/delete/${code}`, { crossDomain: true })
+    .then(res => {
+      list.splice(idx, 1);
+      setList(list);
+      setMessage(res.data)
+    }).catch((err) => {
+      setError(err.data);
+    })
+  }
+
+  const handleEdit = (code, idx, newUrl) => {
+    setError('');
+    setMessage('');
+    axios.put(`${PRODUCTION_BASE_URL}/api/url/update/${code}`, {
+      "longUrl": newUrl
+  }, { crossDomain: true })
+    .then(res => {
+      list[idx].longUrl = newUrl
+      setList(list);
+      setMessage(res.data)
+    }).catch((err) => {
+      setError(err.response.data);
+    })
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Container component="main">
@@ -20,12 +75,16 @@ const UrlListPage = () => {
           alignItems: 'center',
         }}
         >
+      <Stack sx={{ width: '100%', py: 1 }} spacing={2}>
+      {error && <Alert severity="error">{error}</Alert>}
+      {message && <Alert severity="success">{message}</Alert>}
+      </Stack>
       <Box sx={{ backgroundColor: 'primary.main', color: 'white', p: 3, width: '100%' }}>
         <Typography component="h1" variant="h5">
           Your Urls:
         </Typography>
       </Box>
-      <UrlList/>
+      <UrlList list={list} handleDelete={(code, idx) => handleDelete(code, idx)} handleEdit={handleEdit}/>
       </Box>
       </Container>
     </ThemeProvider>
