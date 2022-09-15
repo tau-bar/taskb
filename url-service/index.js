5b9e6ce87e8443239531e82a47b2c14573309cfd
tau-bar <taufiq.b.ar@gmail.com>
2022-09-16 01:26:19 +0800

    Update COTS

--- url-service/index.js
+++ url-service/index.js
@@ -12,7 +12,7 @@ connection.once('open', () => console.log('Database is connected.'))
 connection.on('error', () => console.log('Could not connect to database.'))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
